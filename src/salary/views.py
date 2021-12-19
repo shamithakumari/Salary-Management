396575b3f1a8428a45dc69eff94cd3570e983a98
Swathi <swathiuday2001@gmail.com>
2021-12-19 16:25:42 +0530

    Changed the style of employee salary view page

--- src/salary/views.py
+++ src/salary/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from authenticate.models import Employee
 from .models import Salary,Department, Deduction
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -10,8 +11,13 @@ def details(request):
     # salary_list = Salary.objects.filter(eid=request.user) 
     salary_list = Salary.objects.filter(eid=request.user.id)
     deduction_list = Deduction.objects.filter(eid=request.user.id)
+    emp_details = User.objects.filter(id=request.user.id)
+    emp_details_2 = Employee.objects.get(user=request.user.id)
+
     return render(request, "salary/salary_details.html", 
     {
         'salary_list':salary_list,
         'ded_list': deduction_list,
+        'emp_details': emp_details,
+        'emp_details_2': emp_details_2,
     })
